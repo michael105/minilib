a519b4d449ca18ca366ad0a77660053ba6e38b27
Michael Myer (misc) <misc.myer@zoho.com>
2019-02-09 09:13:42 +0100

    minor changes

--- test/failsave.c
+++ test/failsave.c
@@ -3,6 +3,12 @@
 
 #include "include/minilib_header.h"
 
+
+// Check, whether the testing routines work.
+// This lines intentionally fail.
+// if they don't, there's a serious problem
+// with your system.
+
 int main(){
 		char *c=0;
 		c = 'X';
