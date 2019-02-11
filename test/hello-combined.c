229697e167383375daa09ba164944dfe062f2372
Michael Myer (misc) <misc.myer@zoho.com>
2019-02-11 01:33:54 +0100

    minor changes

---
+++ test/hello-combined.c
@@ -0,0 +1,26 @@
+#define mini_start start
+#define mini_write
+
+#define OSX
+#define X64
+
+#include "../minilibcombined.c"
+
+#if 0
+testhello:
+	$(info Testing makefile hello)
+	./hello 0> results/hello.stdout
+	@diff -q results/hello-expect.stdout results/hello.stdout && echo $RED Pass: hello || echo Fail: hello
+
+ifdef undef
+#endif
+
+int main(){
+		write(stdout, "Hello world!\n", 13);
+		return(0);
+}
+
+#if 0
+endif
+#endif
+
