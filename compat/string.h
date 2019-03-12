cec5b2c60dbf3dd27bfc46f768bc5220c6ef7f22
Michael Myer <misc.myer@zoho.com>
2019-03-12 06:20:07 +0000

    minor changes

--- compat/string.h
+++ compat/string.h
@@ -1,6 +1,22 @@
 #ifndef string_h
 #define string_h
 
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef stdin
+#define stdin 0
+#endif
+#ifndef stdout
+#define stdout 1
+#endif
+#ifndef stderr
+#define stderr 2
+#endif
+
+#include "sys/types.h"
+
 #include "src/mstrcmp.c"
 #include "src/mstrlen.c"
 #include "src/memcpy.c"
