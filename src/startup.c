65a87752f7c559999b8d277ce9894d47a250a084
Michael Myer <misc.myer@zoho.com>
2019-03-19 23:48:36 +0100

    minor changes

--- src/startup.c
+++ src/startup.c
@@ -1,6 +1,6 @@
 // This is "callen" just before main.
 // 
-// dbg("Startup -xx"); // nor arguments allowed here. 
+// dbg("Startup -xx"); // no arguments allowed here. 
 // otherwise argv[] gets confused
 // Or we would have to add some further bloating bytes
 minilib_global_init();
