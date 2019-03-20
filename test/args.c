ad722015726c8e29062e6e09136eb3c87c6b9c9e
Michael Myer <misc.myer@zoho.com>
2019-03-20 02:16:29 +0100

    minor changes

--- test/args.c
+++ test/args.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv, char **env ){
 		printf("argc: %d\n", argc);
 		printf("argv: %s\n", argv[0]);
 		printf("argv[argc-1]: %s\n", argv[argc-1]);
-		for ( int a =0; env[a]; a++ )
+		for ( int a =0; a<10; a++ )
 				printf("env: %s\n", env[a]);
 		return(0);
 }
