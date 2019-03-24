b6f45b94295371f827f6303c1fe92c943dd50f51
Michael Myer <misc.myer@zoho.com>
2019-03-24 09:46:27 +0100

    headerscript nearly finished

--- minilib.h
+++ minilib.h
@@ -1,69 +1,268 @@
-// Include this file in order to use minilib functions.
-// define the names of the functions you'd like to use here,
-// uncomment unwanted functions.
-
-/// Len of buf used by read, mprintf, ..
-//#define mini_buf 4096
-
-#define mini_start
-//#define mini_vsyscalls // vsyscalls.(+ ~92 bytes). 
-// ~twice faster than standard syscalls at my system,linux 32bit
-//#define mini_errno
+/* This file is part of minilib, (c) 2012-2019 Michael Misc Myer.
+misc.myer@zoho.com / www.github.com/michael105
+Licensed under the terms of a BSD 3-clause License.
+Please see the files LICENSE and NOTICE for the exact conditions. */
 
 
+#ifndef included_minilib_h
+#define included_minilib_h
+
+
+#ifdef mini_tcsetattr
+#ifndef mini_ioctl
+#define mini_ioctl
+#endif
+#endif
+
+#ifdef mini_sprintf
+#ifndef mini_write
 #define mini_write
-//#define mini_open // defines also creat
-//#define mini_close
-//#define mini_read
-//#define mini_lseek // defines also ftruncate, fsync
-#define mini_puts
-//#define mini_putchar
-//#define mini_fputc
+#endif
+#endif
 
-//#define mini_mprints
-//#define mini_mprintf
-//#define mini_perror
-//#define mini_msprintf
-//#define mini_mfprintf
-//#define mini_itohex
-//#define mini_itodec  // also conversion %d in printf
-//#define mini_dtodec  // also conversion %d in printf
-//#define mini_atoi
-//#define mini_itobin
-//#define mini_exit
-//#define mini_syscall
-//#define mini_ioctl
-//#define mini_tcgetattr
-//#define mini_tcsetattr
-//#define mini_select 
+#ifdef mini_fprintf
+#ifndef mini_write
+#define mini_write
+#endif
+#endif
+
+#ifdef mini_itodec
+#ifndef mini_uitodec
+#define mini_uitodec
+#endif
+#endif
+
+#ifdef mini_fputs
+#ifndef mini_strlen
+#define mini_strlen
+#endif
+#endif
+
+#ifdef mini_strncpy
+#ifndef mini_memcpy
+#define mini_memcpy
+#endif
+#endif
+
+#ifdef mini_dtodec
+#ifndef mini_uitodec
+#define mini_uitodec
+#endif
+#endif
+
+#ifdef mini_tcgetattr
+#ifndef mini_ioctl
+#define mini_ioctl
+#endif
+#endif
+
+// src/mstrlen.c
+#ifdef mini_strlen
+int strlen(const char*str);
+#endif
+
+// src/itodec.c
+#ifdef mini_uitodec
+int uitodec(unsigned int i, char *buf, int prec, char limiter );
+#endif
+
+// include/write.h
+#ifdef mini_write
+#include "include/write.h"
+#endif
+
+// src/ioctl.c
+#ifdef mini_ioctl
+int ioctl( int fd, unsigned long int request, ... );
+#endif
+
+// src/memcpy.c
+#ifdef mini_memcpy
+void *memcpy( void *d, const void *s, int n );
+#endif
+
+// src/strcat.c
+#ifdef mini_strcat
+char *strcat(char *dest, const char *src );
+#endif
+
+// include/tcgetattr.h
+#ifdef mini_tcgetattr
+#include "sys/ttycom.h"
+#include "include/tcgetattr.h"
+#endif
+
+// src/mprint.c
+#ifdef mini_print
+int print(const char *msg);
+#endif
+
+// src/memcpy.c
+#ifdef mini_strncpy
+char *strncpy(char *dest, const char *src, int n);
+#endif
+
+// include/close.h
+#ifdef mini_close
+#include "include/close.h"
+#endif
+
+// src/open.c
+#ifdef mini_creat
+inline int volatile __attribute__((always_inline)) creat( const char *s, int mode );
+#endif
+
+// src/ioctl.c
+#ifdef mini_ioctl
+int ioctl( int fd, unsigned long int request, ... );
+#endif
+
+// src/mstrcmp.c
+#ifdef mini_strncmp
+int strncmp(const char*c1,const char*c2,int len);
+#endif
+
+// src/memcpy.c
+#ifdef mini_memcpy
+void *memcpy( void *d, const void *s, int n );
+#endif
+
+// src/malloc.c
+#ifdef mini_free
+void volatile free(void* p);
+#endif
+
+// src/memcpy.c
+#ifdef mini_strcpy
+char *strcpy(char *dest, const char *src);
+#endif
+
+// src/isspace.c
+#ifdef mini_isspace
+int isspace(int c);
+#endif
+
+// include/write.h
+#ifdef mini_write
+#include "include/write.h"
+#endif
+
+// src/itohex.c
+#ifdef mini_itohex
+int itohex(int i,char* buf,int padding);
+#endif
+
+// include/read.h
+#ifdef mini_read
+#include "syscall.h"
+#include "include/read.h"
+#endif
+
+// include/tcsetattr.h
+#ifdef mini_tcsetattr
+#include "sys/ttycom.h"
+#include "termios.h"
+#include "include/tcsetattr.h"
+#endif
+
+// include/isprint.h
+#ifdef mini_isprint
+#include "include/isprint.h"
+#endif
+
+// src/atoi.c
+#ifdef mini_atoi
+int atoi(char *c);
+#endif
+
+// include/fputs.h
+#ifdef mini_fputs
+#include "include/fputs.h"
+#endif
+
+// src/itobin.c
+#ifdef mini__itobin
+int _itobin(int i, char*buf, int prec, int groups );
+#endif
+
+// src/msprintf.c
+#ifdef mini_sprintf
+int sprintf(char *buf, const char* fmt, ... );
+#endif
+
+// src/memset.c
+#ifdef mini_memset
+void *memset( void *s, int c, int n);
+#endif
+
+// src/itodec.c
+#ifdef mini_uitodec
+int uitodec(unsigned int i, char *buf, int prec, char limiter );
+#endif
+
+// src/dtodec.c
+#ifdef mini_dtodec
+int dtodec(double d, char* buf, int precision);
+#endif
+
+// src/mstrcmp.c
+#ifdef mini_strcmp
+int strcmp(const char*c1,const char*c2);
+#endif
+
+// src/itodec.c
+#ifdef mini_itodec
+int itodec(int i, char *buf, int prec, char limiter );
+#endif
+
+// src/mstrcmp.c
+#ifdef mini_memcmp
+int memcmp(const void* c1,const void* c2,int len);
+#endif
+
+// src/open.c
+#ifdef mini_open
+int volatile open( const char *s, int flags, ... );
+#endif
+
+// include/select.h
+#ifdef mini_select
+#include "include/select.h"
+#endif
+
+// src/mstrlen.c
+#ifdef mini_strlen
+int strlen(const char*str);
+#endif
+
+// src/mfprintf.c
+#ifdef mini_fprintf
+int fprintf(int fd, const char* fmt, ... );
+#endif
+
+// src/mprint.c
+#ifdef mini_printl
+int printl(const char *msg);
+#endif
+
+// src/memfrob.c
+#ifdef mini_memfrob
+void* memfrob(void* s, unsigned int len);
+#endif
+
+// src/malloc.c
+#ifdef mini_malloc
+void* volatile malloc(int size);
+#endif
+
+// include/fputc.h
+#ifdef mini_fputc
+#include "include/fputc.h"
+#endif
 
 
-//#define mini_memfrob
-//#define mini_mstrcmp 
-//#define mini_mstrlen
+#ifdef INCLUDESRC
+#include "minilib.c"
+#endif
 
-//#define  mini_memset
-//#define  mini_memcpy
-//#define mini_strncpy
-
-//#define mini_isprint
-//#define mini_isprint
-//#define mini_isspace
-
-//#define mini_epoll // TODO:: doesn't ork
-//#define mini_malloc
-//#define mini_print //print,printl
-
-
-//#define powers // ipoweriui, ipoweri
-
-
-// Whether to overwrite the standard calls to printf, ...
-#define mini_overwrite
-
-
-
-
-
-#include "minilib/include/minilib_header.h"
-
+#endif
