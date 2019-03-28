f9c10843310e4bf4b1a9432ac98b2a067add6e96
Michael Myer <misc.myer@zoho.com>
2019-03-28 15:27:46 +0100

    config system

---
+++ scripts/genconfig.sh
@@ -0,0 +1,187 @@
+#
+# Script generated from "make header" / genheaders.pl
+# The functions are callen from minilib.conf 
+# (or another config file)
+# The output of this script generates minilib.conf.h
+# 
+
+export REAL_CC=gcc
+
+CFLAGS="-I. -fno-stack-protector -fdata-sections -fomit-frame-pointer -ffunction-sections -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-const-variable -Wall -nodefaultlibs -static -nostdlib -DMLIB -DX64 -fno-builtin -I$MLIBDIR""compat -I$MLIBDIR""headers/common-links -I$MLIBDIR""headers/common -I$MLIBDIR -I./"
+
+export CFLAGS="$CFLAGS -Wl,-n,-s,--gc-sections,-nostdlib "
+
+#LDFLAGS=-nostdlib -nodefaultlibs --gc-sections -static -n -s $(LSCRIPT) -nostartfiles -O 2
+
+mini_start(){
+		echo "#define mini_start $1"
+}
+mini_exit(){
+		echo "#define mini_exit $1"
+}
+
+ARCH(){
+		echo "#define MINILIB_ARCH $1"
+		export MINI_ARCH=$1
+}
+OS(){
+		echo "#define MINILIB_OS $1"
+		export MINI_OS=$1
+}
+mini_buf(){
+		echo "#define mini_buf $1"
+}
+
+# --- define_functions-start: generate --- #
+mini_memset(){ 
+  echo "#define mini_memset generate" 
+}
+mini_strcpy(){ 
+  echo "#define mini_strcpy generate" 
+}
+mini_strcmp(){ 
+  echo "#define mini_strcmp generate" 
+}
+mini_dup2(){ 
+  echo "#define mini_dup2 generate" 
+}
+mini_read(){ 
+  echo "#define mini_read generate" 
+}
+mini_dtodec(){ 
+  echo "#define mini_dtodec generate" 
+}
+mini_atoi(){ 
+  echo "#define mini_atoi generate" 
+}
+mini_malloc(){ 
+  echo "#define mini_malloc generate" 
+}
+mini_ioctl(){ 
+  echo "#define mini_ioctl generate" 
+}
+mini_itohex(){ 
+  echo "#define mini_itohex generate" 
+}
+mini_memfrob(){ 
+  echo "#define mini_memfrob generate" 
+}
+mini_select(){ 
+  echo "#define mini_select generate" 
+}
+mini_fputc(){ 
+  echo "#define mini_fputc generate" 
+}
+mini_strncmp(){ 
+  echo "#define mini_strncmp generate" 
+}
+mini_creat(){ 
+  echo "#define mini_creat generate" 
+}
+mini_unlink(){ 
+  echo "#define mini_unlink generate" 
+}
+mini_fputs(){ 
+  echo "#define mini_fputs generate" 
+}
+mini_getpid(){ 
+  echo "#define mini_getpid generate" 
+}
+mini_ftruncate(){ 
+  echo "#define mini_ftruncate generate" 
+}
+mini_dup(){ 
+  echo "#define mini_dup generate" 
+}
+mini_memcmp(){ 
+  echo "#define mini_memcmp generate" 
+}
+mini_fsync(){ 
+  echo "#define mini_fsync generate" 
+}
+mini_memcpy(){ 
+  echo "#define mini_memcpy generate" 
+}
+mini_strncpy(){ 
+  echo "#define mini_strncpy generate" 
+}
+mini__itobin(){ 
+  echo "#define mini__itobin generate" 
+}
+mini_lseek(){ 
+  echo "#define mini_lseek generate" 
+}
+mini_close(){ 
+  echo "#define mini_close generate" 
+}
+mini_isprint(){ 
+  echo "#define mini_isprint generate" 
+}
+mini_print(){ 
+  echo "#define mini_print generate" 
+}
+mini_open(){ 
+  echo "#define mini_open generate" 
+}
+mini_rename(){ 
+  echo "#define mini_rename generate" 
+}
+mini_uitodec(){ 
+  echo "#define mini_uitodec generate" 
+}
+mini_itodec(){ 
+  echo "#define mini_itodec generate" 
+}
+mini_gettimeofday(){ 
+  echo "#define mini_gettimeofday generate" 
+}
+mini_tcgetattr(){ 
+  echo "#define mini_tcgetattr generate" 
+}
+mini_sprintf(){ 
+  echo "#define mini_sprintf generate" 
+}
+mini_strcat(){ 
+  echo "#define mini_strcat generate" 
+}
+mini_time(){ 
+  echo "#define mini_time generate" 
+}
+mini_fstat(){ 
+  echo "#define mini_fstat generate" 
+}
+mini_printl(){ 
+  echo "#define mini_printl generate" 
+}
+mini_tcsetattr(){ 
+  echo "#define mini_tcsetattr generate" 
+}
+mini_strlen(){ 
+  echo "#define mini_strlen generate" 
+}
+mini_prints(){ 
+  echo "#define mini_prints generate" 
+}
+mini_def(){ 
+  echo "#define mini_def generate" 
+}
+mini_dup3(){ 
+  echo "#define mini_dup3 generate" 
+}
+mini_write(){ 
+  echo "#define mini_write generate" 
+}
+mini_fprintf(){ 
+  echo "#define mini_fprintf generate" 
+}
+mini_isspace(){ 
+  echo "#define mini_isspace generate" 
+}
+mini_free(){ 
+  echo "#define mini_free generate" 
+}
+mini_mprotect(){ 
+  echo "#define mini_mprotect generate" 
+}
+# --- define_functions-end: generate --- #
+
