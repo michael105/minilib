f1a450922c83bf9bbc433297ce20e88e2314dbf9
Michael Myer <misc.myer@zoho.com>
2019-04-04 12:34:24 +0200

    cleanup. rc 0.1beta (amd64)

--- src/startup.c
+++
@@ -1,8 +0,0 @@
-// This is "callen" just before main.
-// 
-// dbg("Startup -xx"); // no arguments allowed here. 
-// otherwise argv[] gets confused
-// Or we would have to add some further bloating bytes
-#ifdef mini_buf
-ml.mbufsize = mini_buf;
-#endif
