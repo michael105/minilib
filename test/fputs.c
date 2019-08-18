12adce41b0f9150170e06733ae05b461323ea333
Michael Myer <misc.myer@zoho.com>
2019-08-18 16:16:58 +0200

    fputs testcase added

---
+++ test/fputs.c
@@ -0,0 +1,13 @@
+#define mini_start
+#define mini_fputs
+#define INCLUDESRC
+#include "minilib/minilib.h"
+
+
+
+int main(){
+		fputs( "Hello!\n", stdout );
+		fputs( "Hello stderr!\n", stderr );
+
+		return(0);
+}
