5032bc707f23a6a03208dc31388b95ea7813b060
Michael Myer <misc.myer@zoho.com>
2019-08-18 03:44:40 +0200

    renaming. Stripped trailing "m"

---
+++ src/prints.c
@@ -0,0 +1,65 @@
+#ifndef prints_c
+#define prints_c
+//#include "../include/write.h"
+#include "../include/prints.h"
+#include "../include/stdarg.h"
+
+#ifndef mini_prints
+#define mini_prints
+#endif
+
+//TODO: Handle errors of write (ret == -1)
+
+#if 0
+int _prints(char *msg,...){
+		int a;
+		int ret = 0;
+
+		do {
+				a = 0;
+				while (msg[a] != 0 ){
+						a++;
+				}
+				ret += write(1,msg,a);
+				//msg = 0;//va_arg(args,char*);
+				//msg+=4;
+				printf("\nmsg: %u\n",msg);
+				printf("msg: %u\n",&msg);
+				unsigned int i;
+				i = &msg;
+				printf("i: %u\n",i);
+				i+=4;
+				printf("i: %u\n",i);
+				*msg = i;
+				printf("\nmsg: %u\n",msg);
+				printf("msg: %u\n",&msg);
+
+				msg=0;
+		} while ( msg != 0 );
+		return(ret);
+}
+
+#else
+
+//+needs write.h prints.h stdarg.h
+//+depends write
+int _mprints(char *msg,...){
+		va_list args;
+		va_start(args,msg);
+		int a;
+		int ret = 0;
+
+		do {
+				a = 0;
+				while (msg[a] != 0 ){
+						a++;
+				}
+				ret += write(1,msg,a);
+				msg = va_arg(args,char*);
+		} while ( msg != 0 );
+		va_end(args);
+		return(ret);
+}
+#endif
+
+#endif
---
+++ src/sprintf.c
@@ -0,0 +1,377 @@
+#ifndef msprintf_c
+#define msprintf_c
+
+#ifndef mini_write
+#define mini_write
+#endif
+
+
+#ifndef mini_prints
+#define mini_prints
+#endif
+
+#include "../macros/overrun.h"
+//#include "../include/write.h"
+#include "../include/exit.h" // Needed for testing for buffer overrun
+#include "../include/stdarg.h"
+
+
+#undef MINI_TEST_OVERRUN
+#define MINI_TEST_OVERRUN(pos) if ( pos>= size ){ overflow=1; goto msprintfout;}
+
+
+//+ansi stdio.h
+//+depends write
+//+needs exit.h stdarg.h overrun.h
+//+after itohex itoHEX
+//+def
+int vsnprintf(char *buf, size_t size, const char* fmt, va_list args ){
+
+		//va_list args;
+		//va_start(args,fmt);
+		int a = 0;
+		int b = 0;
+		int overflow = 0;
+		int end, padding, c,groups=0;
+		char sep;
+		char tmp[16];
+		char *s;
+
+#if 1
+		while ( fmt[a] != 0){
+				if ( fmt[a] == '%' ){
+						end=0;
+						padding = 0;
+						sep = 0;
+						groups=0;
+						do {
+								a++;
+								if ( (fmt[a] > 47 ) && (fmt[a] < 58 ) ){
+										c = 0;
+										do { 
+												tmp[c] = fmt[a]; 
+												a++;
+												c++;
+										} while ( (fmt[a] > 47 ) && (fmt[a] < 58 ) );
+										tmp[c] = 0;
+#ifdef mini_atoi
+										padding = atoi(tmp);
+#endif
+								}
+								switch (fmt[a]){
+										case '%': 	
+												buf[b] = '%';
+												end=1;
+												b++;
+												MINI_TEST_OVERRUN(b);
+												break;
+#if 1
+										case 'u':
+#ifdef mini_itodec
+												MINI_TEST_OVERRUN(b+13);
+												b = b + uitodec(va_arg(args,unsigned int),&buf[b],padding,sep);
+#endif
+												end=1;
+												break;
+										case 'd':
+#ifdef mini_itodec
+												MINI_TEST_OVERRUN(b+13);
+												b = b + itodec(va_arg(args,int),&buf[b],padding,sep);
+#endif
+												end=1;
+												break;
+#endif
+										case 'f':
+#ifdef mini_dtodec
+												MINI_TEST_OVERRUN(b+27);
+												if ( padding==0 )
+														padding = 9;
+												b = b + dtodec(va_arg(args,double),&buf[b],padding);
+#endif
+												end=1;
+												break;
+										case 'l':
+#ifdef mini_ltodec
+												MINI_TEST_OVERRUN(b+27);
+												b = b + ltodec(va_arg(args,long),&buf[b],padding,sep);
+#endif
+												end=1;
+												break;
+
+										case 39:
+												sep = '.';
+												break;
+										case 'x':
+#ifdef mini_itohex
+												MINI_TEST_OVERRUN(b+8);
+												b = b + itohex(va_arg(args,int),&buf[b],padding);
+#endif
+												end=1;
+												break;
+										case 'X':
+#ifdef mini_itohex
+												MINI_TEST_OVERRUN(b+8);
+												b = b + itoHEX(va_arg(args,int),&buf[b],padding);
+#endif
+												end=1;
+												break;
+
+										case 'b':
+#ifdef mini_itobin
+
+												MINI_TEST_OVERRUN(b+32);
+												b += itobin(va_arg(args,int),&buf[b],padding,groups);
+#endif
+												end=1;
+												break;
+										case '(':
+												a++;
+												if ( (fmt[a] > 47 ) && (fmt[a] < 58 ) ){
+														c = 0;
+														do { 
+																tmp[c] = fmt[a]; 
+																a++;
+																c++;
+														} while ( (fmt[a] > 47 ) && (fmt[a] < 58 ) );
+														a--;
+														tmp[c] = 0;
+#ifdef mini_atoi
+														groups = atoi(tmp);
+#endif
+												}
+												break;
+										case 's':
+												s = va_arg(args,char*);
+												c=0;
+												while(s[c] != 0){
+														buf[b] = s[c];
+														c++;
+														b++;
+														MINI_TEST_OVERRUN(b);
+												}
+												end=1;
+												break;
+										case 'c':
+												buf[b] = va_arg(args,int);
+												b++;
+												end=1;
+												MINI_TEST_OVERRUN(b);
+												break;
+												
+
+
+
+
+
+								}
+
+						} while ((end==0) && (fmt[a+1] != 0 ));
+
+				} else {
+						buf[b] = fmt[a];
+						b++;
+						MINI_TEST_OVERRUN(b);
+				}
+				a++;
+		}
+msprintfout:
+		buf[b] = 0;
+		va_end(args);
+
+		//write( 2, buf, 20 );
+		//return(write(fd,buf,b));
+		if ( overflow )
+				return(-b);
+		else
+				return(b);
+#endif
+}
+
+
+//+depends write vsnprintf
+//+needs stdarg.h
+//+def
+int dprintf( int fd, const char *fmt, ... ){
+		va_list args;
+		va_start(args,fmt);
+		int r = vsnprintf(ml.mbuf, ml.mbufsize, fmt, args);
+		if ( r<0 ){
+				write(2,"Buffer Overrun.\n",16);
+				exit(1);
+		}
+		write( fd, ml.mbuf, r );
+		va_end(args);
+		return(r);
+}
+
+
+//+depends write vsnprintf
+//+needs stdarg.h
+//+def
+int snprintf( char *buf, size_t size, const char *fmt, ... ){
+		va_list args;
+		va_start(args,fmt);
+		int r = vsnprintf(buf, size, fmt, args);
+		va_end(args);
+		return(r);
+}
+
+
+
+/// fprintf
+/// conversions implemented:
+/// %d  
+/// %u
+/// %f (max precision 8 digits, highest possible number: 2^31
+/// %s
+/// %c
+/// %b : binary output
+/// %x/X : hex output
+/// %(
+/// 
+//+header stdio.h
+//+depends write prints dprintf sprintf fileno
+//+needs mini_stdio.h 
+//+after itobin atoi itodec dtodec ltodec itohex
+//+macro fprintf(stream,...)	write(fileno(stdout),ml.mbuf,sprintf(ml.mbuf,__VA_ARGS__))
+
+
+
+#if 0		
+		va_list args;
+		va_start(args,fmt);
+		int a = 0;
+		int b = 0;
+		int end, padding, c,groups;
+		char sep;
+		char tmp[16];
+		char *s;
+
+		while ( fmt[a] != 0){
+				if ( fmt[a] == '%' ){
+						end=0;
+						padding = 0;
+						sep = 0;
+						groups=0;
+						do {
+								a++;
+								if ( (fmt[a] > 47 ) && (fmt[a] < 58 ) ){
+										c = 0;
+										do { 
+												tmp[c] = fmt[a]; 
+												a++;
+												c++;
+										} while ( (fmt[a] > 47 ) && (fmt[a] < 58 ) );
+										tmp[c] = 0;
+#ifdef mini_atoi
+										padding = atoi(tmp);
+#endif
+								}
+
+								switch (fmt[a]){
+										case '%': 	
+												buf[b] = '%';
+												end=1;
+												b++;
+												MINI_TEST_OVERRUN(b);
+												break;
+										case 'u':
+#ifdef mini_itodec
+												MINI_TEST_OVERRUN(b+13);
+												b = b + uitodec(va_arg(args,unsigned int),&ml.mbuf[b],padding,sep);
+#endif
+												end=1;
+												break;
+										case 'd':
+#ifdef mini_itodec
+												MINI_TEST_OVERRUN(b+13);
+												b = b + itodec(va_arg(args,int),&buf[b],padding,sep);
+#endif
+												end=1;
+												break;
+										case 'f':
+#ifdef mini_dtodec
+												MINI_TEST_OVERRUN(b+27);
+												if ( padding==0 )
+														padding = 9;
+												b = b + dtodec(va_arg(args,double),&buf[b],padding);
+#endif
+												end=1;
+												break;
+
+										case 39:
+												sep = '.';
+												break;
+										case 'x':
+										case 'X':
+#ifdef mini_itohex
+												MINI_TEST_OVERRUN(b+8);
+												b = b + itohex(va_arg(args,int),&buf[b],padding);
+#endif
+												end=1;
+												break;
+										case 'b':
+#ifdef mini_itobin
+
+												MINI_TEST_OVERRUN(b+32);
+												b += itobin(va_arg(args,int),&buf[b],padding,groups);
+#endif
+												end=1;
+												break;
+										case '(':
+												a++;
+												if ( (fmt[a] > 47 ) && (fmt[a] < 58 ) ){
+														c = 0;
+														do { 
+																tmp[c] = fmt[a]; 
+																a++;
+																c++;
+														} while ( (fmt[a] > 47 ) && (fmt[a] < 58 ) );
+														a--;
+														tmp[c] = 0;
+#ifdef mini_atoi
+														groups = atoi(tmp);
+#endif
+												}
+												break;
+										case 's':
+												s = va_arg(args,char*);
+												c=0;
+												while(s[c] != 0){
+														buf[b] = s[c];
+														c++;
+														b++;
+														MINI_TEST_OVERRUN(b);
+												}
+												end=1;
+												break;
+										case 'c':
+												buf[b] = va_arg(args,int);
+												b++;
+												end=1;
+												MINI_TEST_OVERRUN(b);
+												break;
+												
+
+
+
+
+
+								}
+
+						} while ((end==0) && (fmt[a+1] != 0 ));
+
+				} else {
+						buf[b] = fmt[a];
+						b++;
+						MINI_TEST_OVERRUN(b);
+				}
+				a++;
+		}
+		buf[b] = 0;
+		va_end(args);
+		//return(write(1,buf,b));
+		return(b);
+}
+#endif
+#endif
