408c62710bc2096e1c1e86a5a1fc018ccdc10f95
Michael Myer <misc.myer@zoho.com>
2019-08-26 05:01:59 +0200

    started directory scanning functions. Copied from musl

---
+++ src/dirent/readdir.c
@@ -0,0 +1,28 @@
+#ifndef readdir_c
+#define readdir_c
+
+#include <dirent.h>
+//typedef char dirstream_buf_alignment_check[1-2*(int)(
+//	offsetof(struct __dirstream, buf) % sizeof(off_t))];
+
+//+def
+struct dirent *readdir(DIR *dir){
+	struct dirent *de;
+	
+	if (dir->buf_pos >= dir->buf_end) {
+		int len = getdents( dir->fd, dir->buf, sizeof dir->buf);
+		if (len <= 0) {
+			if (len < 0 && len != -ENOENT) errno = -len;
+			return 0;
+		}
+		dir->buf_end = len;
+		dir->buf_pos = 0;
+	}
+	de = (void *)(dir->buf + dir->buf_pos);
+	dir->buf_pos += de->d_reclen;
+	dir->tell = de->d_off;
+	return de;
+}
+
+
+#endif
