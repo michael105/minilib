209e841f4ea80662de6b1e5b6746638d13e0688f
micha <micha@laptop>
2020-05-31 00:19:49 +0000

    (minor) Sun May 31 00:19:49 UTC 2020

---
+++ src/sigemptyset.c
@@ -0,0 +1,19 @@
+#ifndef sigemptyset_c
+#define sigemptyset_c
+// copied from musl. 
+
+//+header signal.h
+//+include
+//+def
+int sigemptyset(sigset_t *set){
+		set->__bits[0] = 0;
+		if (sizeof(long)==4 || _NSIG > 65) set->__bits[1] = 0;
+		if (sizeof(long)==4 && _NSIG > 65) {
+				set->__bits[2] = 0;
+				set->__bits[3] = 0;
+		}
+		return 0;
+}
+
+#endif
+
