96ba58d67cc8e4b7785448a640de184da9351011
Michael Myer <misc.myer@zoho.com>
2020-06-17 00:41:35 +0200

    Add missing files

---
+++ examples/hello-scripted.c
@@ -0,0 +1,9 @@
+#!mini-gcc --exec 
+// this file can be compiled and executed by just calling it..
+// The configuration of the functions to be built
+#include "minilib.conf.h"
+
+int main(){
+		write(0, "Hello world!\n", 13);
+		return(0);
+}
