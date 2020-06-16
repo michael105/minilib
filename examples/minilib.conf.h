293b286023e7a3f73544e240e4ec07f7b056ae01
Michael Myer <misc.myer@zoho.com>
2020-06-17 00:41:57 +0200

    Add missing files

---
+++ examples/minilib.conf.h
@@ -0,0 +1,8 @@
+#define mini_start 
+#define mini_exit 
+#define mini_write generate
+#define MINILIB_OS LINUX
+#define MINILIB_ARCH X64
+#define HEADERGUARDS
+#define INCLUDESRC
+#include "minilib.h"
