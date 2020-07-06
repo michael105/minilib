60cf5a83d6df5f0f656a351fda24528a1f783263
micha <micha@laptop>
2020-07-06 22:54:22 +0000

    rewrote kill

--- minicore/base/kill.c
+++ minicore/base/kill.c
@@ -3,183 +3,119 @@ mini_start
 mini_writes
 mini_perror
 mini_errno
-mini_atoi
-# mini_putchar
-mini_print
-mini_strcasecmp
-#mini_tolower
-mini_buf 1024
+mini_prints
+mini_putc
+mini_strcmp
+
+#mini_buf 256
+#mini_printf
+#mini_itodec
+#mini_dtodec
 LDSCRIPT text_and_bss
 shrinkelf
 INCLUDESRC
 return
 #endif
 
+#define SIGMAX 31
+const char* sig[]={ "", "HUP","INT","QUIT","ILL","TRAP",
+		"ABRT","BUS","FPE","KILL","USR1","SEGV","USR2",
+		"PIPE","ALRM","TERM","STKFLT","CHLD","CONT",
+		"STOP","TSTP","TTIN","TTOU","URG","XCPU","XFSZ",
+		"VTALRM","PROF","WINCH","POLL","PWR","SYS"};
 
-#define ARRAY_SIZE(x)	((sizeof(x) / sizeof((x)[0])))
 
-static struct {
-	const char *argv0;
-	int signum;
-} opt;
+int toint(const char *c){
+	int ret = 0;
 
-static const struct sig_entry {
-	const char *name;
-	int signum;
-} sig_table[] = {
-	{ "HUP", 1 },
-	{ "INT", 2 },
-	{ "QUIT", 3 },
-	{ "ILL", 4 },
-	{ "TRAP", 5 },
-	{ "ABRT", 6 },
-	{ "IOT", 6 },
-	{ "BUS", 7 },
-	{ "FPE", 8 },
-	{ "KILL", 9 },
-	{ "USR1", 10 },
-	{ "SEGV", 11 },
-	{ "USR2", 12 },
-	{ "PIPE", 13 },
-	{ "ALRM", 14 },
-	{ "TERM", 15 },
-	{ "STKFLT", 16 },
-	{ "CHLD", 17 },
-	{ "CLD", 17 },
-	{ "CONT", 18 },
-	{ "STOP", 19 },
-	{ "TSTP", 20 },
-	{ "TTIN", 21 },
-	{ "TTOU", 22 },
-	{ "URG", 23 },
-	{ "XCPU", 24 },
-	{ "XFSZ", 25 },
-	{ "VTALRM", 26 },
-	{ "PROF", 27 },
-	{ "WINCH", 28 },
-	{ "IO", 29 },
-	{ "POLL", 29 },
-	{ "PWR", 30 },
-	{ "UNUSED", 31 },
-	{ "SYS", 31 },
-	{ "RTMIN", 34 },
-	{ "RTMAX", 64 },
-};
-
-#define NSIG 64
-
-static int parse_int(const char *str)
-{
-	int val;
-	char *ptr;
-
-	val = atoi(str);
-	return val;
-}
-
-static int get_signum(const char *name)
-{
-	const struct sig_entry *ent;
-	int signum;
-	size_t i;
-
-	signum = parse_int(name);
-	if (0 <= signum && signum <= NSIG)
-		return signum;
-
-	if (!strncasecmp(name, "SIG", 3))
-		name += 3;
-
-	for (i = 0; i < ARRAY_SIZE(sig_table); i++) {
-		ent = &sig_table[i];
-		if (!strcasecmp(ent->name, name))
-			return ent->signum;
+	while ( *c > 47 && *c < 58 ){
+			ret *= 10;
+			ret += (*c-48);
+			*c++;		
 	}
-//	fprintf(stderr, "%s: invalid signal: %s\n", opt.argv0, name);
-	writes("invalid signal: ");
-	writes(name);
-	exit(1);
+	return(ret);
 }
 
-/*
-static void print_table(void)
-{
-	const struct sig_entry *ent;
-	size_t i;
-
-	for (i = 0; i < ARRAY_SIZE(sig_table); i++) {
-		ent = &sig_table[i];
-		//printf("%2d %-7s ", ent->signum, ent->name);
-		if (i % 10 == 9)
-			writes('\n');
-	}
-}
-*/
-
-static void print_names(int signum)
-{
-	const struct sig_entry *ent;
-	size_t i, j;
-
-	j = 0;
-	for (i = 0; i < ARRAY_SIZE(sig_table); i++) {
-		ent = &sig_table[i];
-		if (signum < 0 || signum == ent->signum) {
-			print(ent->name);
-			print(" ");
-			if (j++ == 15) {
-				writes('\n');
-				j = 0;
-			}
-		}
-	}
-}
 
 void usage(){
-		writes("Usage: kill [-SIGNAL] [-l NUMBER] [-L] [PID1] [PID2] ...\n");
+		writes("Usage: kill [-l] [-SIGNAL/-signalnum] [PID1] [PID2] ...\n");
 		exit(1);
 }
 
-/* Usage: kill [-SIGNAL] PID... */
-/* Usage: kill -l [NUMBER] */
-/* Usage: kill -L */
-int main(int argc, const char *argv[])
+void print_names(){
+		for ( int a = 1; a<32; a++ ){
+				prints( sig[a] );
+				putc(' ',stdout);
+		}
+		putc('\n',stdout);
+		exit(0);
+}
+
+int nokill(int p, int s){
+		//printf("nokill: %d, %d\n",p,s);
+		return(0);
+}
+
+int cmp(const char *p, const char *p2 ){
+
+		int *l = (int*)p;
+		int *l2 = (int*)p2;
+
+		if ((*l) == (*l2)) 
+				return(0);
+		return(1);
+}
+
+int to_number( const char *p ){
+		for ( int ret = 1; ret <= SIGMAX; ret ++ ){
+				if ( cmp( p, sig[ret]) == 0 )
+						return(ret);
+		}
+		writes("Unknown Signal: ");
+		prints(p);
+		putc('\n',stdout);
+		exit(1);
+		return(0);
+}
+
+int main(int argc, char *argv[])
 {
-	int signum, ret, i;
+	int signum=SIGTERM; 
 	pid_t pid;
 
 	if (argc == 1) {
 			usage();
 	}
 
-	opt.argv0 = argv[0];
-	opt.signum = SIGTERM;
+#define AV argv[0]
+	for ( *argv++; *argv && argv[0][0]=='-'; *argv++ ){
+			if ( AV[1] == 'h' )
+					usage();
+			if ( AV[1] == 'l' )
+					print_names();
+		
+			if ( AV[1] >= '0' && AV[1] <= '9' )
+					signum = toint( (AV +1 ) );
 
-	if (!strcmp(argv[1], "-L")) {
-		print_names(-1);
-		return 0;
-	} else if (!strcmp(argv[1], "-l")) {
-		if (argc > 2)
-			signum = get_signum(argv[2]);
-		else
-			signum = -1;
+			int tmp =1;
+			if ( AV[1] == 'S' && AV[2] == 'I' && AV[3] == 'G' )
+					tmp = 4;
 
-		print_names(signum);
-		return 0;
-	} else if (argv[1][0] == '-') {
-		opt.signum = get_signum(argv[1] + 1);
-		i = 2;
-	} else {
-		i = 1;
+			if ( AV[tmp] >= 'A' && AV[tmp] <= 'X' ){
+					signum = to_number( AV+tmp );
+			}
+
 	}
 
-	ret = 0;
-	for (; i < argc; i++) {
-		pid = parse_int(argv[i]);
-		if (kill(pid, opt.signum)) {
+	int ret = 0;
+	while (*argv){
+		pid = toint(*argv);
+		if (kill(pid, signum)) {
 				perror("kill: ");
 			ret = 1;
 		}
+		*argv++;
 	}
+
 	return ret;
 }
