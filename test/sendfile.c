3a8ed2391395f2905c26d80af4f29fcfcfaa9aac
Michael Myer <misc.myer@zoho.com>
2020-07-08 02:12:41 +0200

    sendfile

---
+++ test/sendfile.c
@@ -0,0 +1,20 @@
+#if 0
+mini_buf 256
+mini_start
+mini_open
+mini_printf
+mini_itodec
+mini_atoi
+mini_fork
+mini_INCLUDESRC
+return
+#endif
+
+int main(int argc, char**argv){
+		int fd = open( "hello.c", O_RDONLY );
+		long int offset = 20;
+		sendfile(STDOUT_FILENO,fd,&offset,19);
+		while ( sendfile(STDOUT_FILENO,fd,0,16) );
+
+		return(0);
+}
