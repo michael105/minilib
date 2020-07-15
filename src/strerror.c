192330d095a64d8105094b4e8535daaf2d2556da
micha <micha@laptop>
2020-07-15 15:51:15 +0000

    (rebuild) Wed Jul 15 15:51:15 UTC 2020

--- src/strerror.c
+++ src/strerror.c
@@ -2,12 +2,11 @@
 #define strerror_r
 
 
-char *errstr = "error: 000";
-
 //+header string.h
 //+def
 char* strerror( int errnum ){
 #ifndef mini_verbose_errstr
+		static char *errstr = "error: 000";
 		errstr[7] = '0';
 		while ( errnum>99 ){
 				errnum-=100;
@@ -26,11 +25,11 @@ char* strerror( int errnum ){
 }
 
 //+doc convert errno to str, with 3 chars length
-// ending the string (located on the stack (!) 
+// ending the string (located in the bss section)
 // with two \0\0, when errno<100
 //+def
 const char *errno_str(int err){
-		char *e="100\0";
+		static char *e="100\0";
 		char *p = e;
 		if ( err>99 ){
 				err-=100;
