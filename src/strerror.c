bf7c8af4869b7aec6b347633779629c2b927f201
micha <micha@laptop>
2020-07-17 14:02:04 +0000

    (rebuild) Fri Jul 17 14:02:04 UTC 2020

--- src/strerror.c
+++ src/strerror.c
@@ -9,15 +9,20 @@ static char* strerror( int errnum ){
 		static char errstr[18];
 		char* msg = "Error, errno: 000";
 		for ( char *c = errstr; (*c=*msg); c++ && msg++ );
-		while ( errnum>99 ){
+		// code below works. would be faster.
+		// but adds more than 60 Bytes,
+		// and performance might be irrelevant at this location.
+		/*while ( errnum>99 ){
 				errnum-=100;
 				errstr[14]++;
 		}
 		while ( errnum>9 ){
 				errnum-=10;
 				errstr[15]++;
-		}
-		errstr[16] = 48+errnum; // 0+errnum..
+		}*/
+		errstr[14]+=errnum/100;
+		errstr[15]+=(errnum%=100)/10;
+		errstr[16]+=errnum%10; // 0+errnum..
 		return( errstr );
 #else
 		return((char*)verbose_errstr(errnum));
