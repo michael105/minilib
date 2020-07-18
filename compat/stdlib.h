c3f74ed5cc4aac79b7715d2a39008f6d114d2271
Michael Myer <misc.myer@zoho.com>
2020-07-18 02:55:15 +0200

    (minor) Sat Jul 18 02:55:15 CEST 2020

--- compat/stdlib.h
+++ compat/stdlib.h
@@ -156,14 +156,14 @@ static long int labs(long int i);
 #ifdef mini_INCLUDESRC
 
 #include "minilib/src/malloc.c"
-#include "minilib/src/getenv.c"
-#include "minilib/include/math.h"
 #include "minilib/src/strtol.c"
-#include "minilib/src/rand.c"
-#include "minilib/src/system.c"
 #include "minilib/src/atexit.c"
-#include "minilib/src/abort.c"
 #include "minilib/src/atoi.c"
+#include "minilib/include/math.h"
+#include "minilib/src/rand.c"
+#include "minilib/src/abort.c"
+#include "minilib/src/system.c"
+#include "minilib/src/getenv.c"
 
 // Need global included. Doesn't matter by which file.
 #include "src/minilib_global.c"
