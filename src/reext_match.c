fa8ae7694921ae027d90ca5b0e702f9674e0b4d5
micha <micha@laptop>
2020-07-23 15:39:18 +0000

    simple regular expression engine

---
+++ src/reext_match.c
@@ -0,0 +1,76 @@
+//+doc simple regex engine.
+// matches: * for every count of any char
+// ? for 1 char
+// backslash: escape *,?, and backslash itself.
+// \d - digit
+// \D - nondigit
+// \s - space
+// \S - nonspace
+//
+// (memo) When the regex ist defined as C string,
+// a backslash has to be defined as double backslash
+// in the source code.
+//+def
+int reext_match(char *text, char *re){
+		while ( *text!=0 ){
+				switch ( *re ){
+						case '?':
+								break;
+						case '*':
+								re++;
+								if ( *re == 0)
+										return(0); // no chars anymore. so a match
+								for ( ; *text != *re; text++ ){
+										if ( *text==0 )
+												return(-2);
+										if ( match( text,re) == 0 )
+												return(0);
+								}
+								break;
+						case '\\': // match escaped *,?,backslashes
+								*re++;
+								if ( *re == 'd' ){
+										if ( *text>='0' && *text <= '9' )
+												break;
+										else
+												return(-1);
+								}
+								if ( *re == 'D' ){
+										if ( *text>='0' && *text <= '9' )
+												return(-1);
+										else
+												break;
+								}
+								if ( *re == 's' ){
+										if ( isspace(*text) )
+												break;
+										else
+												return(-1);
+								}
+								if ( *re == 'S' ){
+										if ( !isspace(*text) )
+												break;
+										else
+												return(-1);
+								}
+
+						default:
+								if ( *re==0 ) //partial match ( could be spared )
+										return(-2);
+								if ( *re!=*text ){
+										return(-1);
+								}
+				}
+				re++; text++;
+		}
+
+		if ( *re==0 ){
+						return(0); //matched
+		}
+		
+		return(-3); 
+		// regex matched text, but the regex is longer than text
+		// also the case for text==0
+}
+
+
