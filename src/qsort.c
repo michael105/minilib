f55055d5678ce67b47b0a878cada867b02844dad
Michael Myer <misc.myer@zoho.com>
2020-07-29 17:16:39 +0200

    (minor) Wed Jul 29 17:16:38 CEST 2020

--- src/qsort.c
+++ src/qsort.c
@@ -31,17 +31,17 @@
 static inline void swap(void* a, void* b,int size){
 		if ( size==sizeof(int) ){
         asm ("xor %0,%1\nxor %1,%0\nxor %0,%1"
-						:"+r"(*(int*)a),"+m"(*(int*)b) );
+						:"+r"(*(int*)a),"+g"(*(int*)b) );
 				return;
 		}
 		if ( size==sizeof(long) ){
         asm ("xor %0,%1\nxor %1,%0\nxor %0,%1" 
-						:"+r"(*(long*)a),"+m"(*(long*)b) );
+						:"+r"(*(long*)a),"+g"(*(long*)b) );
 				return;
 		}
 		for ( int n=size;n--;){
         asm ("xor %0,%1\nxor %1,%0\nxor %0,%1" 
-						:"+r"(*(char*)a),"+m"(*(char*)b) );
+						:"+r"(*(char*)a),"+g"(*(char*)b) );
 				a++;b++;
 		}
 }
