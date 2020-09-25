e5dfd5319abaae3c2d47d428a4543fcc3cbb77b2
Michael Myer <misc.myer@zoho.com>
2020-09-25 18:53:22 +0000

    getgroups

--- compat/stdlib.h
+++ compat/stdlib.h
@@ -142,15 +142,15 @@ static long int labs(long int i);
 #ifdef mini_INCLUDESRC
 
 #include "minilib/src/qsort.c"
-#include "minilib/src/system.c"
-#include "minilib/src/atexit.c"
-#include "minilib/src/strtol.c"
-#include "minilib/src/getenv.c"
-#include "minilib/src/abort.c"
 #include "minilib/src/atol.c"
+#include "minilib/src/strtol.c"
 #include "minilib/include/math.h"
+#include "minilib/src/getenv.c"
 #include "minilib/src/atoi.c"
+#include "minilib/src/abort.c"
+#include "minilib/src/atexit.c"
 #include "minilib/src/rand.c"
+#include "minilib/src/system.c"
 
 // Need global included. Doesn't matter by which file.
 #include "src/minilib_global.c"
---
+++ src/userdb/getgroups.c
@@ -0,0 +1,35 @@
+//+doc get the groups of the calling process
+//+depends getpwuid setgrent strcmp
+//+def
+int getgroups(int maxgroups, int *list){
+	int uid = getuid();
+	int groups = 0;
+	struct passwd* user = getpwuid(uid);
+	if ( !user )
+		return(-1);
+
+	setgrent(); // set groups to the first entry
+
+	struct group* gr = 0;
+
+	while ( groups<maxgroups ){
+		gr = getgrent();
+		if ( !gr ) // last entry, or error
+			return(groups);
+		int a=0;
+		for ( char* u=*gr->gr_mem;u;u=gr->gr_mem[++a] ){
+			printsl( "u ",u );
+			if ( strcmp( u,user->pw_name ) == 0 ){
+				*list=gr->gr_gid;
+				list++;
+				groups++;
+				break;
+			}
+		}
+	}
+
+#ifdef mini_errno
+	errno=EINVAL;
+#endif
+	return(-1);
+}
