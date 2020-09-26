216360b18079b6d22f406d75fa3b505e8bd0adf3
Michael Myer <misc.myer@zoho.com>
2020-09-26 18:22:43 +0000

    (minor) Sat Sep 26 18:22:43 UTC 2020

--- src/userdb/getgrouplist.c
+++ src/userdb/getgrouplist.c
@@ -1,8 +1,19 @@
+//+doc needs rewrite.
+// now nonstandard.
+//+depends getusergroups
 //+def
 int getgrouplist(const char* user, gid_t group, gid_t *groups, int *ngroups){
+	
+	int gr = getusergroups(user,*ngroups,groups);
 
+	for ( int a = 0; a<gr; a++ ){
+		if ( group == groups[a] )
+			return(gr); // group already within the list
+	}
+	if ( gr>=*ngroups-1 )
+		return(-1);
 
+	groups[gr] = group; // append group
 
-
-	return(0);
+	return(gr+1);
 }
