8aebf5845ebdf36b1a2c8ce8ebc76eae185651a6
Michael Myer <misc.myer@zoho.com>
2020-12-10 19:39:09 +0000

    ap

---
+++ minicore/utils/ap.c
@@ -0,0 +1,115 @@
+#if 0
+mini_start
+mini_open
+mini_close
+mini_write
+mini_writes
+mini_ewrites
+mini_read
+#mini_group_printf
+#mini_buf 1024
+mini_GETOPTS
+#LDSCRIPT text_and_bss
+shrinkelf
+INCLUDESRC
+OPTFLAG -Os
+return
+#endif
+
+
+void writefile(int fd){
+	char buf[4096];
+	int l, len=0;
+	while ( l=read(0,buf,4096) ){
+		write(fd,buf,l);
+		len+=l;
+	}
+	//fprintf(stderr,"wrote: %d\n",len);
+	write(fd,(char*)&len,4);
+	close(fd);
+}
+
+int seekfile(int fd,int trunc){
+	int size = lseek( fd, 0, SEEK_END );
+	int fsize = lseek( fd, size-4, SEEK_SET );
+	int len;
+	int r = read(fd,(char*)&len,4);
+
+	//fprintf(stderr,"len: %d\n",len);
+	if ( r!=4 || ( len > fsize ) ){
+		ewrites("Error. Wrong len.\n");
+		exit(5);
+	}
+
+	lseek( fd, size-4-len, SEEK_SET );
+	if ( trunc )
+		ftruncate( fd,size-4-len );
+	return(len);
+}
+
+
+int main(int argc, char **argv){
+		int opts=0;
+		char buf[4096];
+		int ret = PARSEOPTS_short(opts, argv, (h+r+a+e), SETOPT_short(opts,h));
+
+		if ( GETOPT_short(opts,h) ){
+				writes("Usage: ap [-h] [-r] [-a] [-e] file\n\
+-a append / -r replace / -e extract stdin to file / to stdout\n");
+				return 1;
+		} 
+
+
+		if ( GETOPT_short(opts,a)){
+				int fd = open( argv[ret], O_RDWR | O_APPEND );
+				if ( fd<=0 )
+					return ( 1 );
+
+				int size = lseek( fd, 0, SEEK_END );
+				writefile(fd);
+
+				return(0);
+		} 
+
+		if ( GETOPT_short(opts,r)){
+				int fd = open( argv[ret], O_RDWR );
+				if ( fd<=0 )
+					return ( 1 );
+
+				int len = seekfile(fd,1);
+				writefile(fd);
+				return(0);
+		}
+
+
+
+		if ( GETOPT_short(opts,e)){
+				int fd = open( argv[ret], O_RDONLY );
+				if ( fd<=0 )
+					return ( 1 );
+
+				int len = seekfile(fd,0);
+
+				int l;
+				do {
+					if ( len > 4096 )
+						l=read(fd,buf,4096);
+					else
+						l=read(fd,buf,len);
+
+					len-=l;
+					write(1,buf,l);
+				} while ( len>0 && l );
+
+				if ( len ){
+					ewrites("Error\n");
+					return(3);
+				}
+
+				close(fd);
+				return(0);
+		} 
+
+
+		return 0;
+}
