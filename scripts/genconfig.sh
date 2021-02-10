3d0dd58e105632533441e5b07c36e7b49aab37c2
Michael Myer <misc.myer@zoho.com>
2021-02-10 19:21:28 +0000

    (minor) Wed Feb 10 19:21:28 UTC 2021

--- scripts/genconfig.sh
+++ scripts/genconfig.sh
@@ -90,15 +90,6 @@ DEBUG(){
 
 # Generate debug symbols, including minilib's source.
 FULLDEBUG(){
-		export OPTFLAG='-Og -g -E'
-		export STRIPFLAG=""
-		export opt_shrinkelf=0
-		export fulldebug=1
-}
-
-
-# Generate debug symbols, including minilib's source.
-FULLDEBUG(){
 		export opt_shrinkelf=0
 		export fulldebug=1
 }
