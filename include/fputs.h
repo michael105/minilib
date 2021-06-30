22cb9dbc24c563279616ce70b8bedfd64baa1384
Michael Myer <misc.myer@zoho.com>
2021-06-30 14:36:15 +0000

    Update codeql workflows

--- include/fputs.h
+++ include/fputs.h
@@ -10,7 +10,6 @@
 int strlen(const char*str);
 // after doesnt ork here. inline. so here's the predef.
 // need to rethink the dependency build process
-//static int fileno(FILE*);
 
 //+needs mini_fstream.h
 //+after fileno
