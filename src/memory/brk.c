d26d9574fce1d6b1b92b90bb6b65f4fe786256dc
Michael Myer <misc.myer@zoho.com>
2021-07-31 14:35:59 +0000

    split brk src

--- src/memory/brk.c
+++ src/memory/brk.c
@@ -1,15 +1,3 @@
-#ifndef mini_brk_c
-#define mini_brk_c
-
-//+doc the kernel syscall brk.
-//+def 
-static long sys_brk(unsigned long addr){
-		long ret;
-		syscall1( ret,( __SYSCALL(_brk) | NCONST ), addr );
-		return(ret);
-}
-#define _sys_brk sys_brk
-
 //+doc set the brk to addr
 // return 0 on success.
 // conformant brk, when mini_errno is defined return -1 and set errno.
@@ -32,4 +20,3 @@ static int brk( const void* addr ){
 		return(ret);
 }
 
-#endif
---
+++ src/memory/sys_brk.c
@@ -0,0 +1,11 @@
+
+//+doc the kernel syscall brk.
+//+def 
+static long sys_brk(unsigned long addr){
+		long ret;
+		syscall1( ret,( __SYSCALL(_brk) | NCONST ), addr );
+		return(ret);
+}
+#define _sys_brk sys_brk
+
+
