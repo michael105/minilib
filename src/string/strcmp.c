82da5f61f669d7c22b10354a2aa44bcbd9bfea39
Michael Myer <misc.myer@zoho.com>
2021-09-20 14:44:12 +0000

    splitting sources

--- src/string/strcmp.c
+++ src/string/strcmp.c
@@ -20,47 +20,6 @@ int _strcmp(const char*c1,const char*c2,int len){
 		return(0);
 }
 
-//+inc
-//+depends tolower
-//+needs ctype.h
-//+def
-int _strcasecmp(const char*c1,const char*c2,int len){
-		int a = 0;
-		while ( (c1[a] != 0) && (c2[a]!=0 ) && a != len ){
-				if ( c1[a] != c2[a] ){
-						if ( tolower( c1[a] ) > tolower( c2[a] ) )
-								return(1);
-						if ( tolower( c1[a] ) < tolower( c2[a] ) )
-								return(-1);
-				}
-				a++;
-		}
-		if ( (c1[a] == 0 ) && ( c2[a] == 0 ) )
-				return(0);
-		return (1);
-}
-
-
-//+inc
-//+depends _strcasecmp
-//+needs ctype.h
-//+def
-int strcasecmp(const char*c1,const char*c2){
-		return( _strcasecmp(c1,c2,-1) );
-}
-
-
-//+depends _strcasecmp
-//+inc
-//+def
-int strncasecmp(const char*c1,const char*c2,int len){
-		if ( len <=0 )
-				return(-1);
-		return(_strcasecmp(c1,c2,len) );
-}
-
-
-
 //+depends _strcmp
 //+inc
 //+def
