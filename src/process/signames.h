a4a1d8f9169ce6dce79b66810ca46232fe17e067
Michael Myer <misc.myer@zoho.com>
2021-09-28 09:04:22 +0000

    added bsd signames

---
+++ src/process/signames.h
@@ -0,0 +1,41 @@
+//+doc abbreviated signal names, according to BSD > 4.2
+//+def sys_signame
+const char* sys_signame[] = { 
+"SIG0",
+"HUP",
+"INT",
+"QUIT",
+"ILL",
+"TRAP",
+"ABRT",
+//"IOT", same as abrt
+"BUS",
+"FPE",
+"KILL",
+"USR1",
+"SEGV",
+"USR2",
+"PIPE",
+"ALRM",
+"TERM",
+"STKFLT",
+"CHLD",
+"CONT",
+"STOP",
+"TSTP",
+"TTIN",
+"TTOU",
+"URG",
+"XCPU",
+"XFSZ",
+"VTALRM",
+"PROF",
+"WINCH",
+"IO",
+//"POLL", same as IO
+"PWR",
+"SYS",
+"UNUSED" };
+
+
+
