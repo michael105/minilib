5359d28be3c8f51b2e80db558dfb541e5c043365
Michael Myer <misc.myer@zoho.com>
2021-09-30 10:55:07 +0000

    strncat

---
+++ src/string/strncat.c
@@ -0,0 +1,18 @@
+//+def
+char* strncat( char* dst, const char* src, unsigned int n){
+	if ( !n )
+		return( dst );
+
+	char *d = dst;
+	while ( *d ) // find end
+		d++;
+	while( n && ( *d=*src) ){
+		n--;
+		d++;src++;
+	}
+
+	return(dst);
+}
+
+
+
