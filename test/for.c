7f68cbd6317f332545cc274f1f9de8387342b089
Michael Myer <misc.myer@zoho.com>
2021-10-19 04:26:05 +0000

    add warnings

--- test/for.c
+++ test/for.c
@@ -3,15 +3,20 @@
 . basicconf.conf
 
 mini_strcpy
+mini_gets
 
 return
 #endif
 
 
 int main(){
-	for ( int a=0; a<3; a++ )
+	int a;
+	for ( a=0; a<3; a++ )
 		printf("a: %d\n",a);
 
+	while (a-->0){
+		printf("a: %d\n",a);
+	}
 
 	char c[64];
 	strcpy(c,"12345 Hallo\n");
