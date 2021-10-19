15d54fd7971d94f52d5b44801a93d5a05313a4ed
Michael Myer <misc.myer@zoho.com>
2021-10-19 04:15:15 +0000

    add
    warnings

--- compat/unistd.h
+++ compat/unistd.h
@@ -143,13 +143,13 @@ int getgroups(int maxgroups, int *list);
 
 #ifdef mini_INCLUDESRC
 
-#include "minilib/src/network/gethostname.c"
-#include "minilib/src/exec/_execv.c"
 #include "minilib/src/exec/_execvp.c"
-#include "minilib/src/userdb/getgroups.c"
-#include "minilib/src/exec/execl.c"
-#include "minilib/src/termios/tcgetpgrp.c"
 #include "minilib/src/termios/tcsetpgrp.c"
+#include "minilib/src/exec/execl.c"
+#include "minilib/src/userdb/getgroups.c"
+#include "minilib/src/exec/_execv.c"
+#include "minilib/src/network/gethostname.c"
+#include "minilib/src/termios/tcgetpgrp.c"
 #include "minilib/src/exec/execvp.c"
 
 // Need global included. Doesn't matter by which file.
